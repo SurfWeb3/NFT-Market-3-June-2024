877361eb6a7f5e9b50cf9fe991e753cd894cb31e
unknown <aarkramer@hotmail.com>
2024-03-23 18:24:46 -0700

    23 March 2024 at 18 24

---
+++ pages/index.tsx
@@ -0,0 +1,80 @@
+import type { NextPage } from "next";
+import Link from "next/link";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+
+/**
+ * Landing page with a simple gradient background and a hero asset.
+ * Free to customize as you see fit.
+ */
+const Home: NextPage = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.content}>
+        <div className={styles.hero}>
+          <div className={styles.heroBackground}>
+            <div className={styles.heroBackgroundInner}>
+              <Image
+                src="/hero-gradient.png"
+                width={1390}
+                height={1390}
+                alt="Background gradient from red to blue"
+                quality={100}
+                className={styles.gradient}
+              />
+            </div>
+          </div>
+
+          <div className={styles.heroBodyContainer}>
+            <div className={styles.heroBody}>
+              <h1 className={styles.heroTitle}>
+                <span className={styles.heroTitleGradient}>
+                
+                </span>
+                <br />
+                Wonder Web 3
+              </h1>
+              <p className={styles.heroSubtitle}>
+                <Link
+                  className={styles.link}
+                  href="https://thirdweb.com"
+                  target="_blank"
+                >
+                  
+                </Link>{" "}
+                PLATFORM FOR CURATED DIGITAL ART<b></b> <i></i>
+              </p>
+
+              <div className={styles.heroCtaContainer}>
+                <Link className={styles.heroCta} href="/buy">
+                Gallery
+                </Link>
+                <Link
+                  className={styles.secondaryCta}
+                  href="https://surfweb3.com/about/"
+                  target="_blank"
+                >
+                  Apply to Exhibit
+                </Link>
+              </div>
+            </div>
+          </div>
+          
+          <div className={styles.heroAssetFrame}>
+            <Image
+              src="/hero-asset.png"
+              width={860}
+              height={540}
+              alt="Hero asset, NFT marketplace"
+              quality={100}
+              className={styles.heroAsset}
+            />
+          </div>
+
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
